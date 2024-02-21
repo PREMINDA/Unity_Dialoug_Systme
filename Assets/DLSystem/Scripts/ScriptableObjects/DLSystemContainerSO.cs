3efb488ad12cd3218faf7486d7268c95c9062810
PREMINDA <premindabandara0777@gmail.com>
2024-02-21 22:01:22 +0530

    Container For Store Scriptable Nodes

--- Assets/DLSystem/Scripts/ScriptableObjects/DLSystemContainerSO.cs
+++ Assets/DLSystem/Scripts/ScriptableObjects/DLSystemContainerSO.cs
@@ -4,17 +4,17 @@ using SerializableDictionary;
 namespace DLSystem.Scripts.ScriptableObjects
 {
     
-    public class DLSystemContainerSO
+    public class DLSystemContainerSO : ScriptableObject
     {
         [field: SerializeField] public string FileName { get; set; }
-        [field: SerializeField] public SerializableDictionary<DLSystemDialogueGroupSO, List<DLSystemDialogueSO>> DialogueGroups { get; set; }
+        [field: SerializeField] public SerializableDictionary<DLSystemDialogueGroupSO, List<DLSystemDialogueSO>> GroupDialogues { get; set; }
         [field: SerializeField] public List<DLSystemDialogueSO> UngroupedDialogues { get; set; }
 
         public void Initialize(string fileName)
         {
             FileName = fileName;
 
-            DialogueGroups = new SerializableDictionary<DLSystemDialogueGroupSO, List<DLSystemDialogueSO>>();
+            GroupDialogues = new SerializableDictionary<DLSystemDialogueGroupSO, List<DLSystemDialogueSO>>();
             UngroupedDialogues = new List<DLSystemDialogueSO>();
         }
     }
